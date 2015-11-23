ccab4c5033664a1400bc7c1a38766b1ddbcb19a4
Pete Nicholls <pete@metanation.com>
2015-11-23 18:01:36 +1300

    Route images through HTTPS for iOS 9 restrictions (closes #9)

--- Punky Brewster/Beer.swift
+++ Punky Brewster/Beer.swift
@@ -1,6 +1,8 @@
 import UIKit
 
 class Beer: NSObject {
+    static let HTTPS_IMAGE_DOMAIN = "d3g0s2k979mn74.cloudfront.net"
+    
     var name:String?
     var imageURL:NSURL?
     var abv:Float?
@@ -8,8 +10,16 @@ class Beer: NSObject {
     
     static func fromJSON(properties:[String:AnyObject]) -> Beer {
         let beer = Beer()
+        let imageURLComponents:NSURLComponents = NSURLComponents(string: properties["image_url"] as! String)!
+        
+        // Images must be fetched through HTTPS for iOS 9
+        imageURLComponents.scheme = "https"
+        imageURLComponents.host = HTTPS_IMAGE_DOMAIN
+        
+        print(String(imageURLComponents.URL))
+        
         beer.name = properties["name"] as? String
-        beer.imageURL = NSURL(string: properties["image_url"] as! String)
+        beer.imageURL = imageURLComponents.URL
         beer.abv = properties["abv"] as? Float
         beer.pricePerLitre = properties["price"] as? Float
         return beer
