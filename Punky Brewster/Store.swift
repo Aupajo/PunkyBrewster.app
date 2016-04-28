75e598411b3db18ba16f9926ad63d8448fea7398
Pete Nicholls <pete@metanation.com>
2016-04-28 17:06:31 +1200

    Display user location on map

--- Punky Brewster/Store.swift
+++ Punky Brewster/Store.swift
@@ -6,9 +6,14 @@ class Store: NSObject {
     let address = "22B Tyne Street"
     let location = CLLocation(latitude: -43.5368540539132, longitude: 172.608717675136)
     
+    lazy var annotation:StoreMapAnnotation = {
+        return StoreMapAnnotation(store: self)
+    }()
+    
     var coordinate:CLLocationCoordinate2D {
         return location.coordinate
     }
+    
     
     func regionWithDistance(distance:CLLocationDistance) -> MKCoordinateRegion {
         return MKCoordinateRegionMakeWithDistance(coordinate, distance, distance)
